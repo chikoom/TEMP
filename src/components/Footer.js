c93f40392a7551eff172c0d1db78b3672f04defd
Chikoom <callforcopy@gmail.com>
2020-09-03 10:23:17 +0300

    desgin

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Footer = () => (
+  <footer className='bg-light p-3 text-center'>
+    <div className='logo' />
+    <p>Screenters Team 2020 ©</p>
+  </footer>
+)
+
+export default Footer
